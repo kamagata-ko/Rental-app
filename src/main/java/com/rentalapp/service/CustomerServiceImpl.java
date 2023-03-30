28a8707033106e484bb495c8b96df6462fff74dd
kamagata-ko <eighth.9512@gmail.com>
2023-03-30 23:55:54 +0900

    @Transactional追加

--- src/main/java/com/rentalapp/service/CustomerServiceImpl.java
+++ src/main/java/com/rentalapp/service/CustomerServiceImpl.java
@@ -5,11 +5,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.rentalapp.mapper.CustomerMapper;
 import com.rentalapp.model.CustomerModel;
 
 @Service
+@Transactional
 public class CustomerServiceImpl implements CustomerService {
 
 	@Autowired
