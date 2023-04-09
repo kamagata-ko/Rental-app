f94fd74e007654df4e187fa59b4baf10ed0a9c55
Takayuki-Y5991 <konkon.gitsune628@gmail.com>
2023-04-09 16:31:47 +0900

    Swagger UI 各コントローラーの説明追加

--- src/main/java/com/rentalapp/RentalAppApplication.java
+++ src/main/java/com/rentalapp/RentalAppApplication.java
@@ -1,13 +1,21 @@
 package com.rentalapp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/* SwaggerUiで表示されるタイトル */
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Rental APP",
+                version = "1.0.0",
+                description = "書籍レンタルサービス"))
 public class RentalAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RentalAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RentalAppApplication.class, args);
+    }
 
 }
