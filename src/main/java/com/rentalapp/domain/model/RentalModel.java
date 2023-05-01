651e0e0ea2c7941fd07bc280b8f28f39cb14ce07
kamagata-ko <eighth.9512@gmail.com>
2023-05-01 10:41:29 +0900

    履歴テーブルモデル・SQL新規追加

---
+++ src/main/java/com/rentalapp/domain/model/RentalModel.java
@@ -0,0 +1,21 @@
+package com.rentalapp.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RentalModel {
+    private Integer id;
+    private Integer customerId;
+    private Integer bookId;
+    private LocalDateTime rentalStartDate;
+    private LocalDateTime scheduledReturnDate;
+    private LocalDateTime returnCompletionDate;
+}
