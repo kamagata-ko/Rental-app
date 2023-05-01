23cf7c634ef787ec352d2e3d70de4c72b46c4be4
kamagata-ko <eighth.9512@gmail.com>
2023-05-01 11:45:55 +0900

    コメントアウト（アノテーション）

--- src/main/java/com/rentalapp/application/model/RentalRequest.java
+++ src/main/java/com/rentalapp/application/model/RentalRequest.java
@@ -1,14 +1,9 @@
 package com.rentalapp.application.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+//@Data
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class RentalRequest {
 //    private Integer id;
 //    private Integer customerId;
--- src/main/java/com/rentalapp/domain/model/RentalModel.java
+++ src/main/java/com/rentalapp/domain/model/RentalModel.java
@@ -1,14 +1,9 @@
 package com.rentalapp.domain.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+//@Data
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class RentalModel {
 //    private Integer id;
 //    private Integer customerId;
