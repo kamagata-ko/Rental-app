5bfae786238920b0cba42da07f970fe506c42225
kamagata-ko <eighth.9512@gmail.com>
2023-05-05 06:03:53 +0900

    借りたい本のステータスをレンタル中にする

--- src/main/java/com/rentalapp/domain/repository/RentalRepositoryImpl.java
+++ src/main/java/com/rentalapp/domain/repository/RentalRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.rentalapp.domain.info.RentalInfo;
 import com.rentalapp.infrastructure.helper.TRentalHistoryHelperImpl;
 import com.rentalapp.infrastructure.mapper.BookMapper;
 import com.rentalapp.infrastructure.mapper.RentalHistoryMapper;
+import com.rentalapp.infrastructure.model.TBookModel;
 import com.rentalapp.infrastructure.model.TRentalHistoryModel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -24,9 +25,11 @@ public class RentalRepositoryImpl implements RentalRepository {
         Integer customerId = info.getCustomerId(); // パラメータから顧客IDを取得。
         List<Integer> bookIds = info.getBookId(); // パラメータから本IDを取得。
         LocalDateTime now = LocalDateTime.now(); // 現在時刻を取得。
+        final int STATUS_RENTAL = 1; // レンタル中。
 
         // リクエスト内の本IDの数だけ履歴情報を作成しDBに登録する。
         for (Integer bookId : bookIds) {
+            // 履歴情報作成・追加
             TRentalHistoryModel model = new TRentalHistoryModel();
             model.setCustomerId(customerId);
             model.setBookId(bookId);
@@ -34,9 +37,15 @@ public class RentalRepositoryImpl implements RentalRepository {
             model.setScheduledReturnDate(now); // TODO 返却予定日を１週間後とかにする。（本テーブルに情報持たせてもいいかも）
 
             rentalHistoryMapper.insert(model);
-        }
 
-        //  TODO レンタルした本のステータスを1：レンタル中に変更する。（本テーブル操作。）
+            // 本ステータスをレンタル中に変更
+            TBookModel bm = new TBookModel();
+            bm.setId(bookId);
+            bm.setStatus(STATUS_RENTAL);
+            bm.setUpdateDate(now);
+
+            bookMapper.updateByPrimaryKeySelective(bm);
+        }
 
         return 0;
     }
