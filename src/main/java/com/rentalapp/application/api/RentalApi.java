e048c0f3f9334d967fc79b4581c52781f938be1b
kamagata-ko <eighth.9512@gmail.com>
2023-05-05 10:02:45 +0900

    HTTPメソッド修正

--- src/main/java/com/rentalapp/application/api/RentalApi.java
+++ src/main/java/com/rentalapp/application/api/RentalApi.java
@@ -1,12 +1,12 @@
 package com.rentalapp.application.api;
 
 import com.rentalapp.application.model.RentalRequest;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("rental")
+@RequestMapping("rentals")
 public interface RentalApi {
-    @GetMapping("do-rental")
+    @PostMapping
     int doRental(@RequestBody RentalRequest request);
 }
