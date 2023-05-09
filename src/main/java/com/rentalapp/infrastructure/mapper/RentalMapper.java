1330bcabbd624739acb7c54aa3726e43c515cb29
kamagata-ko <eighth.9512@gmail.com>
2023-05-09 23:39:57 +0900

    未使用メソッドを削除

--- src/main/java/com/rentalapp/infrastructure/mapper/RentalMapper.java
+++ src/main/java/com/rentalapp/infrastructure/mapper/RentalMapper.java
@@ -2,27 +2,8 @@ package com.rentalapp.infrastructure.mapper;
 
 import com.rentalapp.infrastructure.model.TRentalModel;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
 
 @Mapper
 public interface RentalMapper {
     int insert(TRentalModel model);
-
-    /**
-     * 指定したIDによって履歴情報を取得する。
-     *
-     * @param id      （履歴ID・顧客ID・本ID）のいずれか。
-     * @param pattern パターン（1:履歴ID・2:顧客ID・3:本ID）
-     * @return 履歴情報。
-     */
-    List<TRentalModel> selectById(Integer id, int pattern);
-
-    List<TRentalModel> selectAll(
-            @Param("limit") int limit,
-            @Param("offset") int offset,
-            @Param("sort") String sort);
-
-    int delete(Integer id);
 }
